5628c1f668fe350fa3f3cf32e4eb545f2de3bfed
Alex Selby <alexander.selby@mansfield.ox.ac.uk>
2024-10-19 21:08:24 +0100

    Updating dynamic to feature controller

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from controller import PDcontroller
 from terrain import generate_reference_and_limits
 
 
@@ -87,7 +88,7 @@ class Mission:
 
 
 class ClosedLoop:
-    def __init__(self, plant: Submarine, controller):
+    def __init__(self, plant: Submarine, controller: PDcontroller):
         self.plant = plant
         self.controller = controller
 
@@ -104,7 +105,7 @@ class ClosedLoop:
         for t in range(T):
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
-            # Call your controller here
+            actions[t] = self.controller.compute_control_action(mission.reference, observation_t)      # Call controller here
             self.plant.transition(actions[t], disturbances[t])
 
         return Trajectory(positions)
