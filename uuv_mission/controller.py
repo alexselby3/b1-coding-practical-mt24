228ab5178c920ed03cfae35a100cdc5ef0e8e3f0
Alex Selby <alexander.selby@mansfield.ox.ac.uk>
2024-10-20 15:23:29 +0100

    kp=0.2 kd=0.5

--- uuv_mission/controller.py
+++ uuv_mission/controller.py
@@ -1,7 +1,7 @@
 # Implementing a PD controller
 
 class PDcontroller:
-    def __init__(self, KP=0.2, KD=0.7):
+    def __init__(self, KP=0.2, KD=0.5):
         self.KP = KP
         self.KD = KD
         self.previous_error = 0
